c0c1b3b16817827e0d4c7aec7d57cc900340a8ce
EGLOFF Valentin <valentin.egloff@irisa.fr>
2018-06-20 16:35:47 +0200

    Forgot to remove old syscall header

--- include/syscall.h
+++
@@ -1,4 +0,0 @@
-#include "portability.h"
-
-ac_int<32, false> solveSysCall(ac_int<32, false> syscallId, ac_int<32, false> arg1, ac_int<32, false> arg2,ac_int<32, false> arg3,
-                           ac_int<32, false> arg4, ac_int<2, false> *sys_status);
