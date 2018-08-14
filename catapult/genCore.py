034c815dfe8adf72c929ed66aa242bb2440093a7
EGLOFF Valentin <valentin.egloff@irisa.fr>
2018-08-14 13:52:20 +0200

    Cache exploration on catapult

--- catapult/genCore.py
+++ catapult/genCore.py
@@ -77,7 +77,7 @@ directive set -DESIGN_GOAL area
 directive set -OLD_SCHED false
 directive set -SPECULATE true
 directive set -MERGEABLE true
-directive set -REGISTER_THRESHOLD 4096 
+directive set -REGISTER_THRESHOLD 4096
 directive set -MEM_MAP_THRESHOLD 64
 directive set -FSM_ENCODING none
 directive set -REG_MAX_FANOUT 0
@@ -128,7 +128,7 @@ genCore = """directive set /{top}/core/doCore<0U>:core.REG:rsc/MAP_TO_MODULE {{[
 go architect
 """
 
-genCache = """directive set /{top}/cim:rsc -MAP_TO_MODULE ST_singleport_8192x32.ST_SPHD_BB_8192x32m16_aTdol_wrapper
+genCacheCore = """directive set /{top}/cim:rsc -MAP_TO_MODULE ST_singleport_8192x32.ST_SPHD_BB_8192x32m16_aTdol_wrapper
 directive set /{top}/cim:rsc -INTERLEAVE {associativity}
 directive set /{top}/cdm:rsc -MAP_TO_MODULE ST_singleport_8192x32.ST_SPHD_BB_8192x32m16_aTdol_wrapper
 directive set /{top}/cdm:rsc -INTERLEAVE {associativity}
@@ -138,9 +138,9 @@ directive set /{top}/memictrl -WORD_WIDTH 128
 directive set /{top}/memdctrl:rsc -MAP_TO_MODULE ST_singleport_4096x128.ST_SPHD_BB_4096x128m8_aTdol_wrapper
 directive set /{top}/memdctrl -WORD_WIDTH 128
 go architect
-cycle add /{top}/core/core:rlp/main/loadidata:read_mem(cim:rsc(0)(0).@) -from /{top}/core/core:rlp/main/icache:case-0:if:setctrl:read_mem(memictrl:rsc.@) -equal 0
-cycle add /{top}/core/core:rlp/main/dcache:case-4:if:read_mem(cdm:rsc(0)(0).@) -from /{top}/core/core:rlp/main/dcache:case-0:if:setctrl:read_mem(memdctrl:rsc.@) -equal 0
-cycle add /{top}/core/core:rlp/main/dcache:case-0:if:setctrl:read_mem(memdctrl:rsc.@) -from /{top}/core/core:rlp/main/icache:case-0:if:setctrl:read_mem(memictrl:rsc.@) -equal 0
+//cycle add /{top}/core/core:rlp/main/loadidata:read_mem(cim:rsc(0)(0).@) -from /{top}/core/core:rlp/main/icache:case-0:if:setctrl:read_mem(memictrl:rsc.@) -equal 0
+//cycle add /{top}/core/core:rlp/main/dcache:case-4:if:read_mem(cdm:rsc(0)(0).@) -from /{top}/core/core:rlp/main/dcache:case-0:if:setctrl:read_mem(memdctrl:rsc.@) -equal 0
+//cycle add /{top}/core/core:rlp/main/dcache:case-0:if:setctrl:read_mem(memdctrl:rsc.@) -from /{top}/core/core:rlp/main/icache:case-0:if:setctrl:read_mem(memictrl:rsc.@) -equal 0
 """
 
 genCacheonly = """if {{ {cachesize} == 65536 }} {{
@@ -162,14 +162,16 @@ go architect
 //cycle add /{top}/core/core:rlp/main/loadidata:read_mem(cim:rsc(0)(0).@) -from /{top}/core/core:rlp/main/icache:case-0:if:setctrl:read_mem(memictrl:rsc.@) -equal 0
 //cycle add /{top}/core/core:rlp/main/dcache:case-4:if:read_mem(cdm:rsc(0)(0).@) -from /{top}/core/core:rlp/main/dcache:case-0:if:setctrl:read_mem(memdctrl:rsc.@) -equal 0
 //cycle add /{top}/core/core:rlp/main/dcache:case-0:if:setctrl:read_mem(memdctrl:rsc.@) -from /{top}/core/core:rlp/main/icache:case-0:if:setctrl:read_mem(memictrl:rsc.@) -equal 0
-set read_mem [cycle find_op *read_mem* -all true]
-set write_mem [cycle find_op *write_mem* -all true]
-for {{ set i 1 }} {{ $i < [llength $read_mem] }} {{ incr i }} {{
-	cycle add [lindex $read_mem $i-1] -from [lindex $read_mem $i] -equal 0
-}}
-cycle add [lindex $read_mem $i-1] -from [lindex $write_mem 0] -equal 0
-for {{ set i 1 }} {{ $i < [llength $write_mem] }} {{ incr i }} {{
-	cycle add [lindex $write_mem $i-1] -from [lindex $write_mem $i] -equal 0
+if {{ {associativity} != 1 }} {{
+	set read_mem [cycle find_op *read_mem* -all true]
+	set write_mem [cycle find_op *write_mem* -all true]
+	for {{ set i 1 }} {{ $i < [llength $read_mem] }} {{ incr i }} {{
+		cycle add [lindex $read_mem $i-1] -from [lindex $read_mem $i] -equal 0
+	}}
+	cycle add [lindex $read_mem $i-1] -from [lindex $write_mem 0] -equal 0
+	for {{ set i 1 }} {{ $i < [llength $write_mem] }} {{ incr i }} {{
+		cycle add [lindex $write_mem $i-1] -from [lindex $write_mem $i] -equal 0
+	}}
 }}
 """
 
@@ -206,56 +208,161 @@ set CacheSize [list 1024 2048 4096 8192 16384 32768 65536]
 set Policy [list 1 2 3]
 set StrPolicy [list fifo lru random]
 foreach cachesize $CacheSize {
-	if { $cachesize == 65536 } {
-		break
-	}
-	go new
-	solution new
-	solution rename "${cachesize}Direct1x[expr 4*$blocksize]"
-	solution options set /Input/CompilerFlags "-D __CATAPULT__=1 -DSize=$cachesize -DAssociativity=1 -DBlocksize=$blocksize -DPolicy=0"
-	go assembly
-	go architect
-	set read_mem [cycle find_op *read_mem* -all true]
-	set write_mem [cycle find_op *write_mem* -all true]
-	for { set i 1 } { $i < [llength $read_mem] } { incr i } {
-		cycle add [lindex $read_mem $i-1] -from [lindex $read_mem $i] -equal 0
-	}
-	cycle add [lindex $read_mem $i-1] -from [lindex $write_mem 0] -equal 0
-	for { set i 1 } { $i < [llength $write_mem] } { incr i } {
-		cycle add [lindex $write_mem $i-1] -from [lindex $write_mem $i] -equal 0
-	}
-	go extract
+	if { [catch {
+		go new
+		solution new -state initial
+		solution options set /Input/CompilerFlags "-D __CATAPULT__=1 -DSize=$cachesize -DAssociativity=1 -DBlocksize=$blocksize -DPolicy=0"
+		solution options set /Input/SearchPath ../include
+		solution options set ComponentLibs/SearchPath ../memories -append
+		flow package require /SCVerify
+		solution file add ../src/core.cpp -type C++
+		solution file add ../src/reformeddm_sim.cpp -type C++ -exclude True
+		solution file add ../src/cache.cpp -type C++
+		solution file add ../src/simulator.cpp -type C++ -exclude True
+		solution file add ../src/elfFile.cpp -type C++ -exclude True
+		solution file add ../src/portability.cpp -type C++
+		solution rename "${cachesize}Direct1x[expr 4*$blocksize]"
+		directive set -DESIGN_GOAL area
+		directive set -OLD_SCHED false
+		directive set -SPECULATE true
+		directive set -MERGEABLE true
+		directive set -REGISTER_THRESHOLD 4096
+		directive set -MEM_MAP_THRESHOLD 64
+		directive set -FSM_ENCODING none
+		directive set -REG_MAX_FANOUT 0
+		directive set -NO_X_ASSIGNMENTS true
+		directive set -SAFE_FSM false
+		directive set -ASSIGN_OVERHEAD 0
+		directive set -UNROLL no
+		directive set -IO_MODE super
+		directive set -REGISTER_IDLE_SIGNAL false
+		directive set -IDLE_SIGNAL {}
+		directive set -STALL_FLAG false
+		directive set -TRANSACTION_DONE_SIGNAL true
+		directive set -DONE_FLAG {}
+		directive set -READY_FLAG {}
+		directive set -START_FLAG {}
+		directive set -BLOCK_SYNC none
+		directive set -TRANSACTION_SYNC ready
+		directive set -DATA_SYNC none
+		directive set -RESET_CLEARS_ALL_REGS true
+		directive set -CLOCK_OVERHEAD 0.000000
+		directive set -OPT_CONST_MULTS use_library
+		directive set -CHARACTERIZE_ROM false
+		directive set -PROTOTYPE_ROM true
+		directive set -ROM_THRESHOLD 64
+		directive set -CLUSTER_ADDTREE_IN_COUNT_THRESHOLD 0
+		directive set -CLUSTER_OPT_CONSTANT_INPUTS true
+		directive set -CLUSTER_RTL_SYN false
+		directive set -CLUSTER_FAST_MODE false
+		directive set -CLUSTER_TYPE combinational
+		directive set -COMPGRADE fast
+		directive set -DESIGN_HIERARCHY cacheWrapper
+		"""+libraries.format(top="cacheWrapper", period=1.5, halfperiod=0.75)+"""
+		go assembly
+		directive set /cacheWrapper/cim:rsc -MAP_TO_MODULE ST_singleport_8192x32.ST_SPHD_BB_8192x32m16_aTdol_wrapper
+		directive set /cacheWrapper/cim:rsc -INTERLEAVE 1
+		directive set /cacheWrapper/cdm:rsc -MAP_TO_MODULE ST_singleport_8192x32.ST_SPHD_BB_8192x32m16_aTdol_wrapper
+		directive set /cacheWrapper/cdm:rsc -INTERLEAVE 1
+		directive set /cacheWrapper/memictrl:rsc -MAP_TO_MODULE ST_singleport_4096x128.ST_SPHD_BB_4096x128m8_aTdol_wrapper
+		directive set /cacheWrapper/memictrl -WORD_WIDTH 128
+		directive set /cacheWrapper/memdctrl:rsc -MAP_TO_MODULE ST_singleport_4096x128.ST_SPHD_BB_4096x128m8_aTdol_wrapper
+		directive set /cacheWrapper/memdctrl -WORD_WIDTH 128
+		go architect
+		set read_mem [cycle find_op *read_mem* -all true]
+		set write_mem [cycle find_op *write_mem* -all true]
+		for { set i 1 } { $i < [llength $read_mem] } { incr i } {
+			cycle add [lindex $read_mem $i-1] -from [lindex $read_mem $i] -equal 0
+		}
+		cycle add [lindex $read_mem $i-1] -from [lindex $write_mem 0] -equal 0
+		for { set i 1 } { $i < [llength $write_mem] } { incr i } {
+			cycle add [lindex $write_mem $i-1] -from [lindex $write_mem $i] -equal 0
+		}
+		go extract
+	} ] }	{ continue }
 }
 foreach policy $Policy {
 	foreach assoc $Associativity {
 		foreach cachesize $CacheSize {
-			go new
-			solution new
-			solution rename "${cachesize}[lindex $StrPolicy [lsearch $Policy $policy]]${assoc}x[expr 4*$blocksize]"
-			solution options set /Input/CompilerFlags "-D __CATAPULT__=1 -DSize=$cachesize -DAssociativity=$assoc -DBlocksize=$blocksize -DPolicy=$policy"
-			go assembly
-			if { $cachesize == 65536 } {
-				directive set /cacheWrapper/cim:rsc -MAP_TO_MODULE ST_singleport_16384x32.ST_SPHD_BB_16384x32m32_aTdol_wrapper
-				directive set /cacheWrapper/cim:rsc -INTERLEAVE $assoc
-				directive set /cacheWrapper/cdm:rsc -MAP_TO_MODULE ST_singleport_16384x32.ST_SPHD_BB_16384x32m32_aTdol_wrapper
-				directive set /cacheWrapper/cdm:rsc -INTERLEAVE $assoc
-			} else {
-				directive set /cacheWrapper/cim:rsc -MAP_TO_MODULE ST_singleport_8192x32.ST_SPHD_BB_8192x32m16_aTdol_wrapper
-				directive set /cacheWrapper/cim:rsc -INTERLEAVE $assoc
-				directive set /cacheWrapper/cdm:rsc -MAP_TO_MODULE ST_singleport_8192x32.ST_SPHD_BB_8192x32m16_aTdol_wrapper
-				directive set /cacheWrapper/cdm:rsc -INTERLEAVE $assoc
-			}
-			go architect
-			set read_mem [cycle find_op *read_mem* -all true]
-			set write_mem [cycle find_op *write_mem* -all true]
-			for { set i 1 } { $i < [llength $read_mem] } { incr i } {
-				cycle add [lindex $read_mem $i-1] -from [lindex $read_mem $i] -equal 0
-			}
-			cycle add [lindex $read_mem $i-1] -from [lindex $write_mem 0] -equal 0
-			for { set i 1 } { $i < [llength $write_mem] } { incr i } {
-				cycle add [lindex $write_mem $i-1] -from [lindex $write_mem $i] -equal 0
-			}
-			go extract
+			if { [catch {
+				go new
+				solution new -state initial
+				solution rename "${cachesize}[lindex $StrPolicy [lsearch $Policy $policy]]${assoc}x[expr 4*$blocksize]"
+				solution options set /Input/CompilerFlags "-D __CATAPULT__=1 -DSize=$cachesize -DAssociativity=$assoc -DBlocksize=$blocksize -DPolicy=$policy"
+				solution options set /Input/SearchPath ../include
+				solution options set ComponentLibs/SearchPath ../memories -append
+				flow package require /SCVerify
+				solution file add ../src/core.cpp -type C++
+				solution file add ../src/reformeddm_sim.cpp -type C++ -exclude True
+				solution file add ../src/cache.cpp -type C++
+				solution file add ../src/simulator.cpp -type C++ -exclude True
+				solution file add ../src/elfFile.cpp -type C++ -exclude True
+				solution file add ../src/portability.cpp -type C++
+				directive set -DESIGN_GOAL area
+				directive set -OLD_SCHED false
+				directive set -SPECULATE true
+				directive set -MERGEABLE true
+				directive set -REGISTER_THRESHOLD 4096
+				directive set -MEM_MAP_THRESHOLD 64
+				directive set -FSM_ENCODING none
+				directive set -REG_MAX_FANOUT 0
+				directive set -NO_X_ASSIGNMENTS true
+				directive set -SAFE_FSM false
+				directive set -ASSIGN_OVERHEAD 0
+				directive set -UNROLL no
+				directive set -IO_MODE super
+				directive set -REGISTER_IDLE_SIGNAL false
+				directive set -IDLE_SIGNAL {}
+				directive set -STALL_FLAG false
+				directive set -TRANSACTION_DONE_SIGNAL true
+				directive set -DONE_FLAG {}
+				directive set -READY_FLAG {}
+				directive set -START_FLAG {}
+				directive set -BLOCK_SYNC none
+				directive set -TRANSACTION_SYNC ready
+				directive set -DATA_SYNC none
+				directive set -RESET_CLEARS_ALL_REGS true
+				directive set -CLOCK_OVERHEAD 0.000000
+				directive set -OPT_CONST_MULTS use_library
+				directive set -CHARACTERIZE_ROM false
+				directive set -PROTOTYPE_ROM true
+				directive set -ROM_THRESHOLD 64
+				directive set -CLUSTER_ADDTREE_IN_COUNT_THRESHOLD 0
+				directive set -CLUSTER_OPT_CONSTANT_INPUTS true
+				directive set -CLUSTER_RTL_SYN false
+				directive set -CLUSTER_FAST_MODE false
+				directive set -CLUSTER_TYPE combinational
+				directive set -COMPGRADE fast
+				directive set -DESIGN_HIERARCHY cacheWrapper
+				"""+libraries.format(top="cacheWrapper", period=1.5, halfperiod=0.75)+"""
+				go assembly
+				if { $cachesize == 65536 } {
+					directive set /cacheWrapper/cim:rsc -MAP_TO_MODULE ST_singleport_16384x32.ST_SPHD_BB_16384x32m32_aTdol_wrapper
+					directive set /cacheWrapper/cim:rsc -INTERLEAVE $assoc
+					directive set /cacheWrapper/cdm:rsc -MAP_TO_MODULE ST_singleport_16384x32.ST_SPHD_BB_16384x32m32_aTdol_wrapper
+					directive set /cacheWrapper/cdm:rsc -INTERLEAVE $assoc
+				} else {
+					directive set /cacheWrapper/cim:rsc -MAP_TO_MODULE ST_singleport_8192x32.ST_SPHD_BB_8192x32m16_aTdol_wrapper
+					directive set /cacheWrapper/cim:rsc -INTERLEAVE $assoc
+					directive set /cacheWrapper/cdm:rsc -MAP_TO_MODULE ST_singleport_8192x32.ST_SPHD_BB_8192x32m16_aTdol_wrapper
+					directive set /cacheWrapper/cdm:rsc -INTERLEAVE $assoc
+				}
+				directive set /cacheWrapper/memictrl:rsc -MAP_TO_MODULE ST_singleport_4096x128.ST_SPHD_BB_4096x128m8_aTdol_wrapper
+				directive set /cacheWrapper/memictrl -WORD_WIDTH 128
+				directive set /cacheWrapper/memdctrl:rsc -MAP_TO_MODULE ST_singleport_4096x128.ST_SPHD_BB_4096x128m8_aTdol_wrapper
+				directive set /cacheWrapper/memdctrl -WORD_WIDTH 128
+				go architect
+				set read_mem [cycle find_op *read_mem* -all true]
+				set write_mem [cycle find_op *write_mem* -all true]
+				for { set i 1 } { $i < [llength $read_mem] } { incr i } {
+					cycle add [lindex $read_mem $i-1] -from [lindex $read_mem $i] -equal 0
+				}
+				cycle add [lindex $read_mem $i-1] -from [lindex $write_mem 0] -equal 0
+				for { set i 1 } { $i < [llength $write_mem] } { incr i } {
+					cycle add [lindex $write_mem $i-1] -from [lindex $write_mem $i] -equal 0
+				}
+				go extract
+			}]} { continue }
 		}
 	}
 }
@@ -365,7 +472,7 @@ def doCore(doCache, cachesize, associativity, blocksize, policy, explore=False, 
 		core = (header + libraries + genCacheonly).format(**locals())
 	elif doCache:
 		top = "doStep"
-		core = (header + libraries + genCache).format(**locals())
+		core = (header + libraries + genCacheCore).format(**locals())
 	else:
 		top = "doStep"
 		core = (header + libraries + genCore).format(**locals())
@@ -376,6 +483,7 @@ def doCore(doCache, cachesize, associativity, blocksize, policy, explore=False, 
 	
 	if explore:
 		if cacheonly:
+			solutionCacheOnly = (header+libraries+genCacheonly+endScript).format(**locals())
 			core += exploreCache
 		else:
 			core += exploreCore
@@ -384,7 +492,7 @@ def doCore(doCache, cachesize, associativity, blocksize, policy, explore=False, 
 		f.write(core)
 
 	if args.shell:
-		subprocess.check_call(["./catapult.sh", "-shell -f {}.tcl".format(name)])
+		subprocess.check_call(["./catapult.sh", "-shell -f {}.tcl".format(name)], stdout=subprocess.DEVNULL)
 	else:
 		subprocess.check_call(["./catapult.sh", "-f {}.tcl".format(name)])
 
