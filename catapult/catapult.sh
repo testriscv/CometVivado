555093faee6f0a3f337dbb8aed8a8e3a375c5d9c
EGLOFF Valentin <valentin.egloff@irisa.fr>
2018-09-12 10:25:51 +0200

    Updated License for catapult

--- catapult/catapult.sh
+++ catapult/catapult.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export PATH=$PATH:/opt/Catapult-10.0b/Mgc_home/bin
-export LM_LICENSE_FILE=1717@loth3.enssat.fr:1717@license.irisa.fr
+export LM_LICENSE_FILE=1717@flexlm.enssat.fr:1717@loth3.enssat.fr:1717@license.irisa.fr
 catapult -product university $@
 
