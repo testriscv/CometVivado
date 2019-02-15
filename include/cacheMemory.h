1d61ddf5cb949e74e2e52eccc0ddaa13fe1c77b5
Simon Rokicki <simon.rokicki@irisa.fr>
2019-02-15 11:48:35 +0100

    Played with memory cache
    - added cacheMemory.h
    - Modified memory.cpp to implement it (does not build)
    - removed memory.cpp from cmake

---
+++ include/cacheMemory.h
@@ -0,0 +1,48 @@
+/*
+ * cacheMemory.h
+ *
+ *  Created on: 15 févr. 2019
+ *      Author: simon
+ */
+
+#ifndef INCLUDE_CACHEMEMORY_H_
+#define INCLUDE_CACHEMEMORY_H_
+
+#include "memoryInterface.h"
+#include "memory.h"
+
+
+/************************************************************************
+ * 	Following values are templates:
+ * 		- OFFSET_SIZE
+ * 		- TAG_SIZE
+ * 		- SET_SIZE
+ * 		- ASSOCIATIVITY
+ ************************************************************************/
+template<int OFFSET_SIZE, int TAG_SIZE, int SET_SIZE, int ASSOCIATIVITY>;
+
+class CacheMemory: public MemoryInterface {
+protected:
+  IncompleteMemory *nextLevel;
+  ac_int<TAG_SIZE+SET_SIZE, false> cacheMemory[SET_SIZE][ASSOCIATIVITY];
+  ac_int<16, false> age[SET_SIZE][ASSOCIATIVITY];
+
+  ac_int<4, false> cacheState; //Used for the internal state machine
+
+
+public:
+  CacheMemory(IncompleteMemory *nextLevel){
+	  nextLevel = nextLevel;
+	  for (int oneSetElement = 0; oneSetElement<SET_SIZE; oneSetElement++){
+		  for (int oneSet = 0; oneSet < ASSOCIATIVITY; oneSet++){
+			  cacheMemory[oneSetElement][oneSet] = 0;
+			  age[oneSetElement][oneSet] = 0;
+		  }
+	  }
+  }
+
+  void process(ac_int<32, false> addr, memMask mask, memOpType opType, ac_int<32, false> dataIn, ac_int<32, false>& dataOut, bool& waitOut);
+};
+
+
+#endif /* INCLUDE_CACHEMEMORY_H_ */
--- src/memory.cpp
+++ src/memory.cpp
@@ -6,106 +6,111 @@
  */
 
 #include <memory.h>
+#include <cacheMemory.h>
 
 #define OFFSET_SIZE 4
 #define TAG_SIZE 32-3-4
-
+#define ASSOCIATIVITY 4
 #define SET_SIZE 256
 
-CORE_UINT(128+TAG_SIZE) data_set1[256];
-CORE_UINT(128+TAG_SIZE) data_set2[256];
-CORE_UINT(128+TAG_SIZE) data_set3[256];
-CORE_UINT(128+TAG_SIZE) data_set4[256];
+ac_int<128+TAG_SIZE, false> data_set1[256];
+ac_int<128+TAG_SIZE, false> data_set2[256];
+ac_int<128+TAG_SIZE, false> data_set3[256];
+ac_int<128+TAG_SIZE, false> data_set4[256];
 
-CORE_UINT(32) ages[256*4];
+ac_int<32, false> ages[256*4];
 
-CORE_UINT(3) miss;
-CORE_UINT(128+TAG_SIZE) newVal;
-
-void readMemory(CORE_INT(32) address, CORE_UINT(4) byteEnable, CORE_UINT(32) externalMemory[65536], CORE_UINT(32) &dataOut, CORE_UINT(1) &stall){
-
-    while (1) {
-	CORE_UINT(3) place = address.SLC(3, 4);
-	CORE_UINT(TAG_SIZE) tag = address.SLC(TAG_SIZE, 7);
-	CORE_UINT(4) offset = address.SLC(4, 0);
-
-    if (miss == 0){
+ac_int<3, false> miss;
+ac_int<128+TAG_SIZE, false> newVal;
 
 
-	CORE_UINT(128+TAG_SIZE) val1 = data_set1[place];
-	CORE_UINT(128+TAG_SIZE) val2 = data_set2[place];
-	CORE_UINT(128+TAG_SIZE) val3 = data_set3[place];
-	CORE_UINT(128+TAG_SIZE) val4 = data_set4[place];
+void CacheMemory::process(ac_int<32, false> addr,
+		memMask mask,
+		memOpType opType,
+		ac_int<32, false> dataIn,
+		ac_int<32, false>& dataOut,
+		bool& waitOut){
 
-	CORE_UINT(TAG_SIZE) tag1 = val1.SLC(TAG_SIZE, 0);
-	CORE_UINT(TAG_SIZE) tag2 = val2.SLC(TAG_SIZE, 0);
-	CORE_UINT(TAG_SIZE) tag3 = val3.SLC(TAG_SIZE, 0);
-	CORE_UINT(TAG_SIZE) tag4 = val4.SLC(TAG_SIZE, 0);
+	ac_int<3, false> place = addr.slc<3>(4);
+	ac_int<TAG_SIZE, false> tag = addr.slc<TAG_SIZE>(7);
+	ac_int<4, false> offset = addr.slc<4>(0);
 
-	CORE_UINT(1) hit1 = (tag1 == tag);
-	CORE_UINT(1) hit2 = (tag2 == tag);
-	CORE_UINT(1) hit3 = (tag3 == tag);
-	CORE_UINT(1) hit4 = (tag4 == tag);
-
-	CORE_UINT(1) hit = hit1 | hit2 | hit3 | hit4;
-	CORE_UINT(128) value;
-
-	if (hit1)
-		value = val1.SLC(128, TAG_SIZE);
-	if (hit2)
-		value = val2.SLC(128, TAG_SIZE);
-	if (hit3)
-		value = val3.SLC(128, TAG_SIZE);
-	if (hit4)
-		value = val4.SLC(128, TAG_SIZE);
-
-	if (hit){
-		dataOut = value.SLC(32, 8*offset);
-	}
-	else{
-	    miss = 5;
-	    stall = 1;
-	}
-	}
-	else{
-
-        if (miss == 5)
-		  CORE_UINT(128+TAG_SIZE) newVal = tag;
-		//CORE_UINT(32) age1 = ages[4*offset+0];
-		//CORE_UINT(32) age2 = ages[4*offset+1];
-		//CORE_UINT(32) age3 = ages[4*offset+2];
-		//CORE_UINT(32) age4 = ages[4*offset+3];
-
-        if (miss >= 2)
-		  newVal.SET_SLC((miss-2)*8+TAG_SIZE, externalMemory[(address>>2) + (miss-2)]);
-
-        miss--;
-		//if (age1<age2 & age1<age3 & age1<age4)
-		if (miss == 1){
-			data_set1[place] = newVal;
-
-		      int newOffset = TAG_SIZE + 8*offset;
-		      dataOut = newVal.SLC(32, newOffset);
-
-        }
-        
-        if (miss == 0){
-		      stall = 0;
-        }
-	//	if (age2<age1 & age2<age3 & age2<age4)
-		//	data_set2[place] = newVal;
-
-	//	if (age3<age1 & age3<age2 & age3<age4)
-	//		data_set3[place] = newVal;
-
-	//	if (age4<age1 & age4<age2 & age4<age3)
-	//		data_set4[place] = newVal;
-
-    
+	if (miss == 0){
 
 
-	}
-	
-	}
+		ac_int<128+TAG_SIZE, false> val1 = data_set1[place];
+		ac_int<128+TAG_SIZE, false> val2 = data_set2[place];
+		ac_int<128+TAG_SIZE, false> val3 = data_set3[place];
+		ac_int<128+TAG_SIZE, false> val4 = data_set4[place];
+
+		ac_int<TAG_SIZE, false> tag1 = val1.slc<TAG_SIZE>(0);
+		ac_int<TAG_SIZE, false> tag2 = val2.slc<TAG_SIZE>(0);
+		ac_int<TAG_SIZE, false> tag3 = val3.slc<TAG_SIZE>(0);
+		ac_int<TAG_SIZE, false> tag4 = val4.slc<TAG_SIZE>(0);
+
+		bool hit1 = (tag1 == tag);
+		bool hit2 = (tag2 == tag);
+		bool hit3 = (tag3 == tag);
+		bool hit4 = (tag4 == tag);
+
+		bool hit = hit1 | hit2 | hit3 | hit4;
+		ac_int<128, false> value;
+
+		if (hit1)
+			value = val1.slc<128>(TAG_SIZE);
+		if (hit2)
+			value = val2.slc<128>(TAG_SIZE);
+		if (hit3)
+			value = val3.slc<128>(TAG_SIZE);
+		if (hit4)
+			value = val4.slc<128>(TAG_SIZE);
+
+		if (hit){
+			dataOut = value.slc<32>(8*offset);
+		}
+		else{
+		    miss = 5;
+		    stall = 1;
+		}
+		}
+		else{
+
+	        if (miss == 5)
+			  ac_int<128+TAG_SIZE, false> newVal = tag;
+			//CORE_UINT(32) age1 = ages[4*offset+0];
+			//CORE_UINT(32) age2 = ages[4*offset+1];
+			//CORE_UINT(32) age3 = ages[4*offset+2];
+			//CORE_UINT(32) age4 = ages[4*offset+3];
+
+	        if (miss >= 2)
+			  newVal.set_slc((miss-2)*8+TAG_SIZE, externalMemory[(address>>2) + (miss-2)]);
+
+	        miss--;
+			//if (age1<age2 & age1<age3 & age1<age4)
+			if (miss == 1){
+				data_set1[place] = newVal;
+
+			      int newOffset = TAG_SIZE + 8*offset;
+			      dataOut = newVal.slc<32>(newOffset);
+
+	        }
+
+	        if (miss == 0){
+			      stall = 0;
+	        }
+		//	if (age2<age1 & age2<age3 & age2<age4)
+			//	data_set2[place] = newVal;
+
+		//	if (age3<age1 & age3<age2 & age3<age4)
+		//		data_set3[place] = newVal;
+
+		//	if (age4<age1 & age4<age2 & age4<age3)
+		//		data_set4[place] = newVal;
+
+
+
+
+		}
+
 }
 
