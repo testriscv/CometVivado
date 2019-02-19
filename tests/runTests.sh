2aeaa73ecc6f3426da31e1a65cb5839b7fc50b22
Joseph Paturel <joseph.paturel@inria.fr>
2019-02-19 14:02:42 +0100

    Added timeout to CI tests (30s)

--- tests/runTests.sh
+++ tests/runTests.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
 SUBFOLDERS=$(ls -d */)
-COMETSIM=../../build/bin/comet.sim
+COMETSIM="../../build/bin/comet.sim"
+TIMEOUT="30s"
 
 for TEST in $SUBFOLDERS
 do
   echo "started test : " $TEST
   cd $TEST
-  $COMETSIM -f *.riscv* > testOutput
-  if [ $? -ne 0 ]
+  timeout $TIMEOUT $COMETSIM -f *.riscv* > testOutput
+  STATUS=$?
+  if [ $STATUS -ne 0 ]
   then
+    if [ $STATUS -eq 124 ]
+    then
+      echo "comet.sim timed out (possible infinite loop)"
+    else
+      echo "comet.sim crashed"
+    fi
     rm testOutput
-    echo "comet.sim crashed"
     exit 1
   fi
   DIFF=$(diff testOutput expectedOutput)
