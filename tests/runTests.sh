76fdc6a418a7326c7d02aa5a83c9b4f81ecf2e9f
Joseph Paturel <joseph.paturel@inria.fr>
2019-02-19 13:47:05 +0100

    Added crash test for CI

--- tests/runTests.sh
+++ tests/runTests.sh
@@ -8,6 +8,12 @@ do
   echo "started test : " $TEST
   cd $TEST
   $COMETSIM -f *.riscv* > testOutput
+  if [ $? -ne 0 ]
+  then
+    rm testOutput
+    echo "comet.sim crashed"
+    exit 1
+  fi
   DIFF=$(diff testOutput expectedOutput)
   if [ "$DIFF" != "" ]
   then
