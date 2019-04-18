c8c44cd5f0b6b84674f5bde424b88342eb61a048
Joseph Paturel <joseph.paturel@inria.fr>
2019-04-18 09:48:51 +0200

    Got rid of boost dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -2,69 +2,27 @@
 #include <string>
 #include <vector>
 
-#include <boost/program_options.hpp>
-
+#include "CLI11.hpp"
 #include "basic_simulator.h"
 
 
 int main(int argc, char** argv)
 {
-    std::string binaryFile;
+    std::string binaryFile; //assign to default
     std::string inputFile;
     std::string outputFile;
     std::string traceFile;
     std::vector<std::string> benchArgs;
 
-    namespace po = boost::program_options;
-    po::options_description optionsDescription("Options");
-    po::positional_options_description positionalOpts;
-    po::variables_map optionMap;
+    CLI::App app{"Comet RISC-V Simulator"};
+    app.add_option("-f,--file", binaryFile, "Specifies the RISC-V program binary file (elf)")->required();
+    app.add_option("-i,--input", inputFile, "Specifies the input file (used as standard input of the runing program)");
+    app.add_option("-o,--output", outputFile, "Specifies the output file (standard output of the running program)");
+	app.add_option("-t,--trace-file", traceFile, "Specifies trace file for simulator output");
+	app.add_option("-a,--program-args", benchArgs, "Specifies command line arguments for the binary program");
 
-    try{
-        optionsDescription.add_options()
-        ("help,h", "Display help message")
-        ("file,f", po::value<std::string>()->required(), "Specifies the RISCV program binary file")
-        ("input,i", po::value<std::string>(), "Specifies input file")
-        ("output,o", po::value<std::string>(), "Specifies output file")
-        ("trace-file,t", po::value<std::string>(), "Specifies trace file for simulator output")
-        ("program-args,a", po::value<std::vector<std::string> >(), "Specifies command line arguments for the binary program");
+	CLI11_PARSE(app, argc, argv);
 
-        positionalOpts.add("program-args", -1);
-
-        po::command_line_parser parser{argc, argv};
-        parser.options(optionsDescription);
-        parser.positional(positionalOpts);
-        po::store(parser.run(), optionMap);
-
-        if(optionMap.count("help")){
-            std::cout << "Usage: "<< argv[0] << " -f FILE [options] [-a [program-args]]\n" << optionsDescription;
-            return 0;
-        }
-
-        po::notify(optionMap);
-
-        binaryFile = optionMap["file"].as<std::string>();
-        if(optionMap.count("program-args")){
-            auto pargs = optionMap["program-args"].as<std::vector<std::string> >();
-            benchArgs.push_back(binaryFile);
-            for(auto a: pargs){
-                benchArgs.push_back(a);
-            }
-        }
-
-        if(optionMap.count("input"))
-          inputFile = optionMap["input"].as<std::string>();
-        if(optionMap.count("output"))
-          outputFile = optionMap["output"].as<std::string>();
-        if(optionMap.count("trace-file"))
-          traceFile = optionMap["trace-file"].as<std::string>();
-
-    }catch(std::exception& e){
-        std::cerr << "Error: " << e.what() << "\n";
-        std::cout << "Usage: "<< argv[0] << " -f FILE [options] [-a [program-args]]\n" << optionsDescription;
-        return -1;
-    }
-    
     BasicSimulator sim(binaryFile.c_str(), benchArgs, inputFile.c_str(), outputFile.c_str(), traceFile.c_str());
 
     sim.run();
